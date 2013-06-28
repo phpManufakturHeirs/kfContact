b18e286d2d4fab07d746f3153237be273b0105ef
hertsch <ralf.hertsch@phpmanufaktur.de>
2013-06-28 15:29:58 +0200

    extended validate()

--- Control/ContactCompany.php
+++ Control/ContactCompany.php
@@ -11,13 +11,47 @@
 
 namespace phpManufaktur\Contact\Control;
 
-use Silex\Application;
-
 class ContactCompany extends ContactParent
 {
 
-    public function validate($data, $options=array())
+    public function validate(&$company_data, $contact_data=array(), $option=array())
     {
+
+        // the company_id must be always set!
+        if (!isset($company_data['company_id'])) {
+            $this->setMessage("Missing the %identifier%! The ID should be set to -1 if you insert a new record.",
+                array('%identifier%' => 'company_id'));
+            return false;
+        }
+
+        // check if any value is NULL
+        foreach ($company_data as $key => $value) {
+            if (is_null($value)) {
+                switch ($key) {
+                    case 'contact_id':
+                    case 'primary_address_id':
+                    case 'primary_person_id':
+                    case 'primary_phone_id':
+                    case 'primary_email_id':
+                    case 'primary_note_id':
+                        $company_data[$key] = -1;
+                        break;
+                    case 'company_name':
+                    case 'company_department':
+                    case 'company_additional':
+                    case 'company_additional_2':
+                    case 'company_additional_3':
+                        $company_data[$key] = '';
+                        break;
+                    case 'company_status':
+                        $company_data[$key] = 'ACTIVE';
+                        break;
+                    default:
+                        throw new ContactException("The key $key is not defined!");
+                        break;
+                }
+            }
+        }
         return true;
     }
 }
