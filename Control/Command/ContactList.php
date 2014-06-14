2b358536f53baa8c4a79aef8076c9002bd87976f
hertsch <ralf.hertsch@phpmanufaktur.de>
2014-06-15 01:32:27 +0200

    start with kitCommand ~~ contact action[list] ~~

---
+++ Control/Command/ContactList.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Contact
+ *
+ * @author Team phpManufaktur <team@phpmanufaktur.de>
+ * @link https://addons.phpmanufaktur.de/event
+ * @copyright 2014 Ralf Hertsch <ralf.hertsch@phpmanufaktur.de>
+ * @license MIT License (MIT) http://www.opensource.org/licenses/MIT
+ */
+
+namespace phpManufaktur\Contact\Control\Command;
+
+use Silex\Application;
+use phpManufaktur\Basic\Control\kitCommand\Basic;
+
+class ContactList extends Basic
+{
+
+    public function ControllerList(Application $app)
+    {
+        echo $app['contact']->getAccessType(10000);
+        return __METHOD__;
+    }
+}
